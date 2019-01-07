50d05cba610ca16fcfb0744f539dead4f250ec98
LAPTOP-IDTFHOLK\koran <koranrg02@gmail.com>
2019-01-07 16:40:09 -0600

    Dominios de modelo y marca

--- OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/domain/Brand.java
+++ OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/domain/Brand.java
@@ -1,4 +1,40 @@
 package com.tvoseguridadelectronica.oss.domain;
 
-public class Brand {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Brand")
+public class Brand implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Brand(String name) {
+        this.name = name;
+    }
+
+    public Brand() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/domain/Model.java
+++ OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/domain/Model.java
@@ -1,4 +1,36 @@
 package com.tvoseguridadelectronica.oss.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Model")
 public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Model(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
