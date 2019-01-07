636e28f99d4dcc064cc2eb736f635117800ae454
TITO\Tito <tito.badilla@hotmail.com>
2019-01-07 10:48:30 -0600

    Inicialización de API REST

---
+++ OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/OssapirestApplication.java
@@ -0,0 +1,14 @@
+package com.tvoseguridadelectronica.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssapirestApplication.class, args);
+	}
+
+}
+
