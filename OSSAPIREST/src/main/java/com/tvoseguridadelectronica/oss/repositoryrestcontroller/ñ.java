e833e7f62feebb8998ce8c7a25cf07042ecf5c64
LAPTOP-IDTFHOLK\koran <koranrg02@gmail.com>
2019-01-07 13:01:51 -0600

    cambios en los paquetes

--- OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/repositoryrestcontroller/ñ.java
+++
@@ -1,4 +0,0 @@
-package com.tvoseguridadelectronica.oss.repositoryrestcontroller;
-
-public class ñ {
-}
---
+++ OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/restcontroller.java
@@ -0,0 +1,4 @@
+package com.tvoseguridadelectronica.oss;
+
+public class restcontroller {
+}
