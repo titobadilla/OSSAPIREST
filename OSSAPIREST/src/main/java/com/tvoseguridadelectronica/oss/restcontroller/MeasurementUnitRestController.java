df5b7152e0b44d94f4891412583a541c8b7d09a7
DESKTOP-FVM8UDG\Diana Bonilla <dianapamela3197@gmail.com>
2019-01-07 16:04:42 -0600

    Device, Tool y material rest controller

--- OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/restcontroller/MeasurementUnitRestController.java
+++ OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/restcontroller/MeasurementUnitRestController.java
@@ -1,4 +1,53 @@
 package com.tvoseguridadelectronica.oss.restcontroller;
 
+import com.tvoseguridadelectronica.oss.domain.MeasurementUnit;
+import com.tvoseguridadelectronica.oss.domain.MeasurementUnit;
+import com.tvoseguridadelectronica.oss.jparepository.MeasurementUnitJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/api/measurementunit"})
 public class MeasurementUnitRestController {
+
+    @Autowired
+    private MeasurementUnitJpaRepository measurementUnitJpaRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<MeasurementUnit>> listAllMeasurementUnit() {
+        List<MeasurementUnit> measurementUnits = measurementUnitJpaRepository.findAll();
+        return new ResponseEntity<List<MeasurementUnit>>(measurementUnits, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MeasurementUnit> createMeasurementUnit(@RequestBody final MeasurementUnit measurementUnit) {
+        measurementUnitJpaRepository.save(measurementUnit);
+        return new ResponseEntity<MeasurementUnit>(measurementUnit, HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MeasurementUnit> editMeasurementUnit(@RequestBody final MeasurementUnit measurementUnit) {
+        measurementUnitJpaRepository.saveAndFlush(measurementUnit);
+        return new ResponseEntity<MeasurementUnit>(measurementUnit, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MeasurementUnit> findMeasurementUnitById(@PathVariable("id") final int id ) {
+
+        MeasurementUnit measurementUnit = measurementUnitJpaRepository.findById(id).get();
+        return new ResponseEntity<MeasurementUnit>(measurementUnit, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MeasurementUnit> deleteMeasurementUnit(@PathVariable("id") final int id) {
+        measurementUnitJpaRepository.deleteById(id);
+
+        return new ResponseEntity<MeasurementUnit>(HttpStatus.NO_CONTENT);
+    }
 }
