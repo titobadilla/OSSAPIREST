eabe52a9132c7852fd8b9d42f2bb66113ec933b5
DESKTOP-FVM8UDG\Diana Bonilla <dianapamela3197@gmail.com>
2019-03-22 13:03:54 -0600

    cambios en wo

--- OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/restcontroller/KitWorkOrderRestController.java
+++ OSSAPIREST/src/main/java/com/tvoseguridadelectronica/oss/restcontroller/KitWorkOrderRestController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*",maxAge = 3600)
 @RestController
-@RequestMapping({"api/listworkorder"})
+@RequestMapping({"api/kitworkorder"})
 public class KitWorkOrderRestController {
 
     @Autowired
